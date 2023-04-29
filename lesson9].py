8f420f415f8d54d32614be6bbd1cd9b1dfdb4445
student <zlata.kramarenko2000@gmail.com>
2023-04-29 10:14:55 +0300

    lesson9

--- lesson9].py
+++ lesson9].py
@@ -1,10 +1,16 @@
 import cv2
 
-image = cv2.imread('humanlesson9.jpg')
+image = cv2.imread('human1.jpg')
 print(image)
 
-casc = cv2.CascadeClassifier
-eyes = cv2.detectMultiScale(image)
+casc = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+eyes = casc.detectMultiScale(image)
+
+print(eyes)
+
+for (x,y,w,h) in eyes:
+    cv2.rectangle(image, (x,y), (x+w, y+h), (0, 0, 255), 3)
 
 cv2.imshow('Human', image)
 
